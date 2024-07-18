43507d2d050181c38789c00269cd16cc88fa1779
amir <amir1378khalili@gmail.com>
2024-07-18 13:16:29 +0330

    ADD celery config

---
+++ shop/celery_conf.py
@@ -0,0 +1,22 @@
+import os
+from datetime import timedelta
+
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.settings")
+
+
+celery_app = Celery("shop")
+celery_app.autodiscover_tasks()
+
+
+celery_app.conf.broker_url = "ampq://rabbitmq"
+celery_app.conf.result_backend = "rpc://"
+celery_app.conf.task_serializer = "json"
+celery_app.conf.result_serializer = "json"
+celery_app.conf.accept_content = ["json", "pickle"]
+celery_app.conf.result_expire = timedelta(days=1)
+celery_app.conf.task_always_eager = False
+celery_app.conf.worker_prefetch_multiplier = 4
+ 
+ 
