13450998ec731d089942a22745c87c42be9329cd
amirkhalili <amir1378khalili@gmail.com>
2024-07-18 18:26:45 +0330

    ENHANCE celery conf

--- shop/celery_conf.py
+++ shop/celery_conf.py
@@ -5,18 +5,14 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.settings")
 
-
 celery_app = Celery("shop")
 celery_app.autodiscover_tasks()
 
-
-celery_app.conf.broker_url = "ampq://rabbitmq"
+celery_app.conf.broker_url = "amqp://"
 celery_app.conf.result_backend = "rpc://"
 celery_app.conf.task_serializer = "json"
-celery_app.conf.result_serializer = "json"
+celery_app.conf.result_serializer = "pickle"
 celery_app.conf.accept_content = ["json", "pickle"]
-celery_app.conf.result_expire = timedelta(days=1)
+celery_app.conf.result_expires = timedelta(days=1)
 celery_app.conf.task_always_eager = False
 celery_app.conf.worker_prefetch_multiplier = 4
- 
- 
