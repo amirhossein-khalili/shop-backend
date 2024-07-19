8a911975974544df7a9dfdb8bc1744c4d18c362d
amirkhalili <amir1378khalili@gmail.com>
2024-07-19 20:39:15 +0330

    ADD delete method bucket

--- file/urls.py
+++ file/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "file"
 urlpatterns = [
-    path("list/", views.BucketView.as_view(), name="list"),
+    path("list/", views.BucketListView.as_view(), name="list"),
+    path("delete/<str:pk>", views.BucketDeleteView.as_view(), name="delete"),
 ]
--- file/views.py
+++ file/views.py
@@ -11,9 +11,27 @@ from . import tasks
 from .tasks import all_bucket_objects_task
 
 
-class BucketView(APIView):
+class BucketListView(APIView):
     permission_classes = [IsAdminUser]
 
     def get(self, request):
         result = tasks.all_bucket_objects_task()
         return Response({"bucket_items": result})
+
+
+class BucketDeleteView(APIView):
+
+    permission_classes = [IsAdminUser]
+
+    def delete(self, request, key):
+
+        tasks.delete_object_task.delay(key)
+
+        return Response({"message": "your object will be delete soon."})
+
+
+# class DownloadBucketObject(IsAdminUserMixin, View):
+#     def get(self, request, key):
+#         tasks.download_object_task.delay(key)
+#         messages.success(request, "your download will start soon.", "info")
+#         return redirect("home:bucket")
